85d6a50fe19257bc2acb56647f7a3dea7d6ed6f0
AtoosaNasiri-Seneca <atoosa.nasiri@senecapolytechnic.ca>
2024-01-27 13:18:41 -0500

    chor: adds scripts

--- scripts/caa900_logo.sh
+++ scripts/caa900_logo.sh
@@ -1,7 +1,5 @@
 # Logo bitmap for linux terminal from 
 # https://patorjk.com/software/taag/#p=testall&f=Big%20Money-sw&t=CAA900%0A
-
-
 echo "    (     (       (         )     )     )  "
 echo "    )\    )\      )\     ( /(  ( /(  ( /(  "
 echo "  (((_)((((_)( ((((_)(   )\()) )\()) )\()) "
